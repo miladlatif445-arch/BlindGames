1a5e6efea47d92d2d9f5b36914b0bad99e57a738
miladlatif445-arch <miladlatif445@gmail.com>
2026-07-17 02:20:42 +0300

    Create Navigation.kt

---
+++ Navigation.kt
@@ -0,0 +1,9 @@
+package com.blindgames.app.navigation
+
+import androidx.compose.runtime.Composable
+import com.blindgames.app.ui.MainScreen
+
+@Composable
+fun BlindGamesNavigation() {
+    MainScreen()
+}
