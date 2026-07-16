8d222492e8f3b1f9d1d25d12e13c6df7d544a705
miladlatif445-arch <miladlatif445@gmail.com>
2026-07-17 02:40:49 +0300

    Create DominoTile.kt

---
+++ app/src/main/java/com/blindgames/app/ui/app/src/main/java/com/blindgames/app/models/DominoTile.kt
@@ -0,0 +1,28 @@
+package com.blindgames.app.models
+
+data class DominoTile(
+
+    val left: Int,
+
+    val right: Int,
+
+    var played: Boolean = false
+
+) {
+
+    fun total(): Int {
+        return left + right
+    }
+
+    fun isDouble(): Boolean {
+        return left == right
+    }
+
+    fun flipped(): DominoTile {
+        return DominoTile(
+            right,
+            left,
+            played
+        )
+    }
+}
